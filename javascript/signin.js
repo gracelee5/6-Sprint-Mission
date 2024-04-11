06cb881f935d7ed5ba0a6ee43dae219d8d07ba26
gracelee5 <113881477+gracelee5@users.noreply.github.com>
2024-04-11 14:38:52 +0900

    feat : 이메일 체크 로직 변경

--- javascript/signin.js
+++ javascript/signin.js
@@ -2,90 +2,89 @@ let isEmailValid = false;
 let isPasswordValid = false;
 
 /*이메일 설정 */
-const emailInput = document.getElementById('email');
-const emailError = document.getElementById('emailError');
+const emailInput = document.getElementById("email");
+const emailError = document.getElementById("emailError");
 
-emailInput.addEventListener('blur',function(){
+emailInput.addEventListener("blur", function () {
   const email = emailInput.value.trim();
 
-  if(!email) {
-    displayEmailError('이메일을 입력해주세요.');
+  if (!email) {
+    displayEmailError("이메일을 입력해주세요.");
     isEmailValid = false;
-  } else if (!email.includes('@') || !email.includes('.')) {
-      displayEmailError('잘못된 이메일 형식입니다.');
-      isEmailValid = false;
+  } else if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email)) {
+    displayEmailError("잘못된 이메일 형식입니다.");
+    isEmailValid = false;
   } else {
-      clearEmailError();
-      isEmailValid = true;
+    clearEmailError();
+    isEmailValid = true;
   }
   manageLoginButton();
 });
 
 function displayEmailError(message) {
-  emailInput.classList.add('error');
+  emailInput.classList.add("error");
   emailError.textContent = message;
-  emailError.style.display = 'block';
+  emailError.style.display = "block";
 }
 
 function clearEmailError() {
-  emailInput.classList.remove('error');
-  emailError.textContent = '';
-  emailError.style.display = 'none';
+  emailInput.classList.remove("error");
+  emailError.textContent = "";
+  emailError.style.display = "none";
 }
 
-
 /*비밀번호 설정 */
-const passwordInput = document.getElementById('password');
-const passwordError = document.getElementById('passwordError');
+const passwordInput = document.getElementById("password");
+const passwordError = document.getElementById("passwordError");
 
-passwordInput.addEventListener('blur',function(){
+passwordInput.addEventListener("blur", function () {
   const password = passwordInput.value.trim();
 
-  if(!password) {
-    displayPasswordError('비밀번호를 입력해주세요.');
+  if (!password) {
+    displayPasswordError("비밀번호를 입력해주세요.");
     isPasswordValid = false;
-  } else if (password.length <8) {
-      displayPasswordError('비밀번호를 8자 이상 입력해주세요.');
-      isPasswordValid = false;
+  } else if (password.length < 8) {
+    displayPasswordError("비밀번호를 8자 이상 입력해주세요.");
+    isPasswordValid = false;
   } else {
-      clearPasswordError();
-      isPasswordValid = true;
+    clearPasswordError();
+    isPasswordValid = true;
   }
   manageLoginButton();
 });
 
 function displayPasswordError(message) {
-  passwordInput.classList.add('error');
+  passwordInput.classList.add("error");
   passwordError.textContent = message;
-  passwordError.style.display = 'block';
+  passwordError.style.display = "block";
 }
 
 function clearPasswordError() {
-  passwordInput.classList.remove('error');
-  passwordError.textContent = '';
-  passwordError.style.display = 'none';
+  passwordInput.classList.remove("error");
+  passwordError.textContent = "";
+  passwordError.style.display = "none";
 }
 
-const loginButton = document.getElementById('loginButton');
+const loginButton = document.getElementById("loginButton");
 loginButton.disabled = true;
 function manageLoginButton() {
-    if (isEmailValid && isPasswordValid) {
-        loginButton.disabled = false;
-    } else {
-        loginButton.disabled = true;
-    }
+  if (isEmailValid && isPasswordValid) {
+    loginButton.disabled = false;
+  } else {
+    loginButton.disabled = true;
+  }
 }
-loginButton.addEventListener('click', function() {
-  window.location.href = '../index.html';
+loginButton.addEventListener("click", function () {
+  window.location.href = "../index.html";
 });
 
-const passwordVisibility = document.getElementById('passwordVisibility');
-passwordVisibility.addEventListener('click',function(){
-  if (passwordInput.type === 'password'){
-    passwordInput.type = 'text';
-    passwordVisibility.src = '../images/eyeon.svg';
+const passwordVisibility = document.getElementById("passwordVisibility");
+passwordVisibility.addEventListener("click", function () {
+  if (passwordInput.type === "password") {
+    passwordInput.type = "text";
+    passwordVisibility.src = "../images/eyeon.svg";
   } else {
-      passwordInput.type = 'password';
-      passwordVisibility.src = '../images/eyeoff.svg';
+    passwordInput.type = "password";
+    passwordVisibility.src = "../images/eyeoff.svg";
   }
 });
