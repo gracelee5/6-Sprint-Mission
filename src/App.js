a8182ee1c75b5a4af01f03b360a507d640008d3b
gracelee5 <113881477+gracelee5@users.noreply.github.com>
2024-04-12 15:26:55 +0900

    babel plugin impory issue

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 import Header from "./components/Header";
 import Products from "./components/Products";
 
